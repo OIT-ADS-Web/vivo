8f16f2273a692443690a2b4fed14fa9073cd3195
Jim Wood <jim.wood@duke.edu>
2016-02-22 16:29:14 -0500

    FDP-2558: Add DocumentModifier for Person/Dept Facet
    Create custom DocumentModifier to create a helper solr field that
    supports hierarchical faceting by department. Bases on this from the
    Solr Wiki:
    
    https://wiki.apache.org/solr/HierarchicalFaceting#Approaches_to_Hierarchical_Facets_in_Solr
    
    Build a list of tree navigations for each person's position representing
    each level of the tree with depth index.
    
    Add to multi-valued custom field department_facet_string

---
+++ src/edu/duke/oit/vivo/webapp/search/documentBuilding/DukeDocumentModifiers.java
@@ -0,0 +1,43 @@
+package edu.duke.oit.vivo.webapp.search.documentBuilding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.DatasetFactory;
+
+import edu.cornell.mannlib.vitro.webapp.dao.ModelAccess;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFServiceFactory;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.impl.RDFServiceUtils;
+
+import edu.cornell.mannlib.vitro.webapp.search.documentBuilding.DocumentModifier;
+import edu.duke.oit.vivo.webapp.search.documentBuilding.PersonDepartmentFacet;
+
+public class DukeDocumentModifiers implements javax.servlet.ServletContextListener{
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        ServletContext context = sce.getServletContext();
+        RDFServiceFactory rdfServiceFactory = RDFServiceUtils.getRDFServiceFactory(context);
+
+        Dataset dataset = DatasetFactory.create(ModelAccess.on(context).getJenaOntModel());
+
+        List<DocumentModifier> modifiers = (List<DocumentModifier>)context.getAttribute("DocumentModifiers");
+        if( modifiers == null ){
+            modifiers = new ArrayList<DocumentModifier>();
+            context.setAttribute("DocumentModifiers", modifiers);
+        }
+
+        modifiers.add( new PersonDepartmentFacet( rdfServiceFactory ));
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent arg0) {
+        // do nothing.
+    }
+}
