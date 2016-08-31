d8dd31cf6d7085a467e66d0d32340650ca5b5d15
Rob Nelson <rn47@duke.edu>
2016-08-31 10:18:08 -0400

    FDP-2738: add vivo:assignedBy to ALLTEXT for grants

--- src/edu/duke/oit/vivo/webapp/search/documentBuilding/DukeGrantFields.java
+++ src/edu/duke/oit/vivo/webapp/search/documentBuilding/DukeGrantFields.java
@@ -7,29 +7,51 @@ import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFServiceFactory;
 import edu.cornell.mannlib.vitro.webapp.search.documentBuilding.ContextNodeFields;
 
 /*
- * SELECT (str(?rawResult) as ?result)
+
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+PREFIX obo: <http://purl.obolibrary.org/obo/> 
+PREFIX core: <http://vivoweb.org/ontology/core#>
+PREFIX vitro: <http://vitro.mannlib.cornell.edu/ns/vitro/0.7#>
+    
+SELECT (CONCAT(
+ ?personLabel, ' ',
+ COALESCE(?assignedByLabel, '')
+) as ?result)
 WHERE {
   <https://scholars.duke.edu/individual/gra35440> a core:Grant .
   <https://scholars.duke.edu/individual/gra35440> core:relates ?role .
   ?role a core:ResearcherRole .
   ?role obo:RO_0000052 ?person .
-  ?person rdfs:label ?rawResult
+  ?person rdfs:label ?personLabel .
+  ?role core:relatedBy ?agreement .
+  OPTIONAL { 
+   ?agreement core:assignedBy ?assignedBy .
+   ?assignedBy rdfs:label ?assignedByLabel
+  } 
 }
 
 
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
 PREFIX obo: <http://purl.obolibrary.org/obo/> 
-PREFIX core: <http://vivoweb.org/ontology/core#> 
+PREFIX core: <http://vivoweb.org/ontology/core#>
 PREFIX vitro: <http://vitro.mannlib.cornell.edu/ns/vitro/0.7#>
-   
-SELECT (str(?rawResult) as ?result)
+  
+SELECT (CONCAT(
+   ?agreementLabel, ' ',
+   COALESCE(?assignedByLabel, '')
+) as ?result)
 WHERE {
-  <https://scholars.duke.edu/individual/per0714252> obo:RO_0000053 ?role .
+  <https://scholars.duke.edu/individual/per1544252> obo:RO_0000053 ?role .
   ?role a core:ResearcherRole .
   ?role core:relatedBy ?agreement .
-  ?agreement rdfs:label ?rawResult .
-} 
+  ?agreement rdfs:label ?agreementLabel .
+  OPTIONAL { 
+   ?agreement core:assignedBy ?assignedBy .
+   ?assignedBy rdfs:label ?assignedByLabel
+  } 
+}
 
 */
 
@@ -48,29 +70,42 @@ public class DukeGrantFields extends ContextNodeFields {
     public DukeGrantFields(RDFServiceFactory rdfServiceFactory){                
         super(queries,rdfServiceFactory);        
     }
-    
+
     private static String queryForPerson =        
-          prefix +
-          "SELECT (str(?rawResult) as ?result) WHERE \n" + 
-          " {\n" +
-          " ?uri a core:Grant . \n" +
-          " ?uri core:relates ?role . \n" +
-          " ?role a core:ResearcherRole . \n" +
-          " ?role obo:RO_0000052 ?person . \n" +
-          " ?person rdfs:label ?rawResult \n" +
-          "}";
-
+          prefix
+          + "SELECT (CONCAT(\n"
+          + "  ?agreementLabel, ' ', \n"
+          + "  COALESCE(?assignedByLabel, '') \n"
+          + ") as ?result)\n"
+          + "WHERE {\n"
+          + "  ?uri obo:RO_0000053 ?role . \n"
+          + "  ?role a core:ResearcherRole . \n"
+          + "  ?role core:relatedBy ?agreement . \n"
+          + "  ?agreement rdfs:label ?agreementLabel . \n"
+          + "  OPTIONAL { \n"
+          + "    ?agreement core:assignedBy ?assignedBy . \n"
+          + "    ?assignedBy rdfs:label ?assignedByLabel \n"
+          + "  }\n" 
+          + "}";
+         
     private static String queryForGrants =        
-          prefix +
-
-          "SELECT (str(?rawResult) as ?result) WHERE \n" +
-          " {\n" +
-          " ?uri obo:RO_0000053 ?role . \n" +
-          " ?role a core:ResearcherRole . \n" +
-          " ?role core:relatedBy ?agreement . \n" +
-          " ?agreement rdfs:label ?rawResult . \n" +
-          "}"; 
-
+          prefix
+          + "SELECT (CONCAT(\n" 
+          + "?personLabel, ' ', \n"
+          + "COALESCE(?assignedByLabel, '') \n"
+          + ") as ?result) \n"
+          + "WHERE { \n"
+          + "  ?uri a core:Grant . \n"
+          + "  ?uri core:relates ?role . \n"
+          + "  ?role a core:ResearcherRole . \n"
+          + "  ?role obo:RO_0000052 ?person . \n"
+          + "  ?person rdfs:label ?personLabel . \n"
+          + "  ?role core:relatedBy ?agreement . \n"
+          + "  OPTIONAL {\n"
+          + "    ?agreement core:assignedBy ?assignedBy . \n"
+          + "    ?assignedBy rdfs:label ?assignedByLabel \n"
+          + "  } \n"
+          + "}";
 
     static List<String> queries = new ArrayList<String>();
     
