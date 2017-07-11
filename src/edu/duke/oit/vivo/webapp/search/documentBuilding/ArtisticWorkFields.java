f66fd065c0ef7c00f3348b405c72141c6208ca02
Rob Nelson <rn47@duke.edu>
2017-07-11 08:30:23 -0400

    FDP-2963: roll up second artistic work indexer SPARQL query into single query since dukeart:roles are already a single value (even when multiple roles)

--- src/edu/duke/oit/vivo/webapp/search/documentBuilding/ArtisticWorkFields.java
+++ src/edu/duke/oit/vivo/webapp/search/documentBuilding/ArtisticWorkFields.java
@@ -10,42 +10,24 @@ import edu.cornell.mannlib.vitro.webapp.search.documentBuilding.ContextNodeField
 /*
  *
 
-# ********* 1st query:
-
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX core: <http://vivoweb.org/ontology/core#>
 PREFIX dukeart: <http://vivo.duke.edu/vivo/ontology/duke-art-extension#> 
 PREFIX bibo:     <http://purl.org/ontology/bibo/>
 
-SELECT (CONCAT(?label, ' ', ?collaborators, ' ', ?workType, ' ', ?abstract) as ?result)
+SELECT (CONCAT(?label, ' ', ?collaborators, ' ', ?roles, ' ', ?workType, ' ', ?abstract) as ?result)
 WHERE {
   ?workUri a dukeart:ArtisticWork . 
   ?workUri dukeart:collaboratorList ?collaborators .
   ?workUri rdfs:label ?label .
-   OPTIONAL { ?workUri  dukeart:workType ?workType. } 
-   OPTIONAL { ?workUri bibo:abstract ?abstract. }
+  ?workUri core:relatedBy ?relatedBy . 
+  ?relatedBy dukeart:roles ?roles . 
+  OPTIONAL { ?workUri  dukeart:workType ?workType. } 
+  OPTIONAL { ?workUri bibo:abstract ?abstract. }
   FILTER(?workUri = <https://scholars.duke.edu/individual/art14240>)
 }
  
-
-# ******* second query:
-
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX core: <http://vivoweb.org/ontology/core#>
-PREFIX dukeart: <http://vivo.duke.edu/vivo/ontology/duke-art-extension#> 
-PREFIX bibo:     <http://purl.org/ontology/bibo/>
-
-SELECT (group_concat(distinct ?roles) as ?role)
-WHERE {
-  ?workUri a dukeart:ArtisticWork . 
-  ?workUri core:relatedBy ?relatedBy . 
-  ?relatedBy dukeart:roles ?roles . 
-  FILTER(?workUri = <https://scholars.duke.edu/individual/art14240>)
-} 
-group by ?roles
-
 */
 
 
@@ -69,31 +51,22 @@ public class ArtisticWorkFields extends DukeContextNodeFields {
  
     private static String queryForWorks =        
           prefix
-          + "SELECT (CONCAT(?label, ' ', ?collaborators, ' ', ?workType, ' ', ?abstract) as ?result) \n"
+          + "SELECT (CONCAT(?label, ' ', ?collaborators, ' ', ?roles, ' ', \n"
+          + "        ?workType, ' ', ?abstract) as ?result) \n"
           + "WHERE { \n"
           + "  ?workUri a dukeart:ArtisticWork . \n" 
           + "  ?workUri dukeart:collaboratorList ?collaborators . \n"
           + "  ?workUri rdfs:label ?label . \n"
+          + "  ?workUri core:relatedBy ?relatedBy . \n" 
+          + "  ?relatedBy dukeart:roles ?roles . \n"
           + "  OPTIONAL { ?workUri  dukeart:workType ?workType. } \n" 
           + "  OPTIONAL { ?workUri bibo:abstract ?abstract. } \n"
           + "  FILTER(?workUri = ?uri) \n"
           + "}"; 
 
-    private static String queryForRoles = 
-          prefix
-          + "SELECT (group_concat(distinct ?roles) as ?role) \n"
-          + "WHERE { \n"
-          + "  ?workUri a dukeart:ArtisticWork . \n" 
-          + "  ?workUri core:relatedBy ?relatedBy . \n"
-          + "  ?relatedBy dukeart:roles ?roles . \n"
-          + "  FILTER(?workUri = ?uri) \n"
-          + "} \n"
-          + "group by ?roles";
-
     static List<String> queries = new ArrayList<String>();
     
     static{
         queries.add( queryForWorks );
-        queries.add( queryForRoles );
     }
 }
